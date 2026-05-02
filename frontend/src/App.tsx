4003ee45e6e014f37c2648da5c674332d4db6b6a
Nikolay <nikolaydon2@gmail.com>
2026-05-02 08:55:58 +0300

    Received data from backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,31 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-
+type User = {
+  id: number;
+  name: string;
+};
 function App() {
+const [data, setData] = useState<User[]>([]);
+ useEffect(() => {
+    axios.get<User[]>('http://localhost:3000/testing')
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }, []);
 
   return (
     <>
-
+{
+  data.map((user: User) => (
+    <div key={user.id}>
+      <p>{user.name}</p>
+    </div>
+  ))
+}
     </>
   )
 }
