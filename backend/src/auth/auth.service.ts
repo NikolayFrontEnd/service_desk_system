e4939cb02448f1796fdc8a6499a6ce6cca70f800
Nikolay <nikolaydon2@gmail.com>
2026-05-06 12:28:54 +0300

    Wrote service and controller

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,6 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from '../prisma/prisma.service';
+import { LoginDto } from './login.dto';
 
 @Injectable()
 export class AuthService {
-    
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async login(loginDto: LoginDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: loginDto.email,
+      },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid email or password');
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(
+      loginDto.password,
+      user.passwordHash,
+    );
+
+    if (!isPasswordCorrect) {
+      throw new UnauthorizedException('Invalid email or password');
+    }
+
+    const payload = {
+      sub: user.id,
+      role: user.role,
+    };
+
+    const accessToken = await this.jwtService.signAsync(payload);
+
+    return {
+      message: 'Login successful',
+      accessToken,
+      user: {
+        id: user.id,
+        name: user.name,
+        role: user.role,
+      },
+    };
+  }
 }
