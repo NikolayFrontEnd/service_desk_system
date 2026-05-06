edcc86a26e0f6a11efd317f1157178395e5cb16c
Nikolay <nikolaydon2@gmail.com>
2026-05-06 10:50:57 +0300

    Added dto

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -1,1 +1,25 @@
-export class CreateUserDto {}
+export type UserRole = 'EMPLOYEE' | 'SUPPORT' | 'TECHNICIAN';
+
+export class CreateUserDto {
+  name: string;
+  email: string;
+  password: string;
+  role: UserRole;
+
+  employee?: {
+    department: string;
+    floor: number;
+    room: number;
+  };
+
+  technician?: {
+    specializations: string;
+    skillLevel: number;
+    experienceYears: number;
+    currentStatus: string;
+    currentLoadMinutes: number;
+    activeTasksCount: number;
+    maxDailyLoadMinutes: number;
+    currentDepartment: string;
+  };
+}
