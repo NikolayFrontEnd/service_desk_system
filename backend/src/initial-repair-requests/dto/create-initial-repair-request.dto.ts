c9c58bcdd42d49bc6aa9fce1b63cc16ab8c0f146
Nikolay <nikolaydon2@gmail.com>
2026-05-07 19:14:23 +0300

    Added dto

--- backend/src/initial-repair-requests/dto/create-initial-repair-request.dto.ts
+++ backend/src/initial-repair-requests/dto/create-initial-repair-request.dto.ts
@@ -1,1 +1,12 @@
-export class CreateInitialRepairRequestDto {}
+export type WorkImpact =
+  | 'CANNOT_WORK'
+  | 'PARTIALLY_CAN_WORK'
+  | 'CAN_WAIT'
+  | 'NOT_URGENT';
+
+export class CreateInitialRepairRequestDto {
+  department!: string;
+  floor!: number;
+  room!: number;
+  workImpact!: WorkImpact;
+}
