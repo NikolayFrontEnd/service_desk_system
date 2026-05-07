34921457199daf466079328470f64a41e2f4881f
Nikolay <nikolaydon2@gmail.com>
2026-05-07 19:15:23 +0300

    Added controller

--- backend/src/initial-repair-requests/initial-repair-requests.controller.ts
+++ backend/src/initial-repair-requests/initial-repair-requests.controller.ts
@@ -1,34 +1,19 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { InitialRepairRequestsService } from './initial-repair-requests.service';
 import { CreateInitialRepairRequestDto } from './dto/create-initial-repair-request.dto';
-import { UpdateInitialRepairRequestDto } from './dto/update-initial-repair-request.dto';
 
 @Controller('initial-repair-requests')
 export class InitialRepairRequestsController {
-  constructor(private readonly initialRepairRequestsService: InitialRepairRequestsService) {}
+  constructor(
+    private readonly initialRepairRequestsService: InitialRepairRequestsService,
+  ) {}
 
   @Post()
-  create(@Body() createInitialRepairRequestDto: CreateInitialRepairRequestDto) {
-    return this.initialRepairRequestsService.create(createInitialRepairRequestDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.initialRepairRequestsService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.initialRepairRequestsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateInitialRepairRequestDto: UpdateInitialRepairRequestDto) {
-    return this.initialRepairRequestsService.update(+id, updateInitialRepairRequestDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.initialRepairRequestsService.remove(+id);
+  createInitialRepairRequest(
+    @Body() createInitialRepairRequestDto: CreateInitialRepairRequestDto,
+  ) {
+    return this.initialRepairRequestsService.createInitialRepairRequest(
+      createInitialRepairRequestDto,
+    );
   }
 }
