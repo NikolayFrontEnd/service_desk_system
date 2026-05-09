c340410cf7152160d028a2890bd49c50510be9f0
Nikolay <nikolaydon2@gmail.com>
2026-05-09 09:36:51 +0300

    Imported AuthModule

--- backend/src/initial-repair-requests/initial-repair-requests.module.ts
+++ backend/src/initial-repair-requests/initial-repair-requests.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { InitialRepairRequestsService } from './initial-repair-requests.service';
 import { InitialRepairRequestsController } from './initial-repair-requests.controller';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [InitialRepairRequestsController],
   providers: [InitialRepairRequestsService],
 })
