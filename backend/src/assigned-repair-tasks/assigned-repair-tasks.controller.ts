9723a0e05d23827bf7efd74344257ab0a75fd4a0
Nikolay <nikolaydon2@gmail.com>
2026-05-09 09:40:26 +0300

    Protected assigned controller

--- backend/src/assigned-repair-tasks/assigned-repair-tasks.controller.ts
+++ backend/src/assigned-repair-tasks/assigned-repair-tasks.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Param, ParseIntPipe, Patch, Post, Delete } from '@nestjs/common';
+import { Body, Controller, Param, ParseIntPipe, Patch, Post, Delete, UseGuards } from '@nestjs/common';
 import { AssignedRepairTasksService } from './assigned-repair-tasks.service';
 import { CreateAssignedRepairTaskDto } from './dto/create-assigned-repair-task.dto';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('assigned-repair-tasks')
+@UseGuards(JwtAuthGuard)
 export class AssignedRepairTasksController {
   constructor(
     private readonly assignedRepairTasksService: AssignedRepairTasksService,
