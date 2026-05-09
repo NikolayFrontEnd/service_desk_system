40942cecbbe2dc8ea5f8f8677b229804f775f841
Nikolay <nikolaydon2@gmail.com>
2026-05-09 09:39:14 +0300

    Imported AuthModule

--- backend/src/assigned-repair-tasks/assigned-repair-tasks.module.ts
+++ backend/src/assigned-repair-tasks/assigned-repair-tasks.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AssignedRepairTasksService } from './assigned-repair-tasks.service';
 import { AssignedRepairTasksController } from './assigned-repair-tasks.controller';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [AssignedRepairTasksController],
   providers: [AssignedRepairTasksService],
 })
