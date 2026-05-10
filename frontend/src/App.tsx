bbb355f4cc084a053fdd27b8aba3ba09e53a8889
Nikolay <nikolaydon2@gmail.com>
2026-05-10 16:09:58 +0300

    Set up page navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
+import { BrowserRouter,Routes, Route } from 'react-router-dom'
 import MainPage from './view/pages/mainPage'
 import SignInPage from './view/pages/signInPage'
 function App() {
 
   return (
     <>
-       <SignInPage />
-     
- <MainPage />
+<BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SignInPage />} />
+        <Route path="/main" element={<MainPage />} />
+      </Routes>
+    </BrowserRouter>
  </>
   )
 }
