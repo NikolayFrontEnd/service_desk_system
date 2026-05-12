9d012a1440e7f120826e97a725d9432dbb2c30fd
Nikolay <nikolaydon2@gmail.com>
2026-05-12 09:54:27 +0300

    Fixed bugs

--- frontend/src/domain/entities/InitialRepairRequest.ts
+++ frontend/src/domain/entities/InitialRepairRequest.ts
@@ -1,11 +1,23 @@
 export class InitialRepairRequest {
+  private _id: number;
+  private _department: string;
+  private _floor: number;
+  private _room: number;
+  private _workImpact: string;
+
   constructor(
-    private _id: number,
-    private _department: string,
-    private _floor: number,
-    private _room: number,
-    private _workImpact: string
-  ) {}
+    id: number,
+    department: string,
+    floor: number,
+    room: number,
+    workImpact: string
+  ) {
+    this._id = id;
+    this._department = department;
+    this._floor = floor;
+    this._room = room;
+    this._workImpact = workImpact;
+  }
 
   get id(): number {
     return this._id;
--- frontend/src/domain/entities/Technician.ts
+++ frontend/src/domain/entities/Technician.ts
@@ -1,12 +1,26 @@
 export class Technician {
+  private _id: number;
+  private _userId: number;
+  private _name: string;
+  private _specializations: string;
+  private _skillLevel: number;
+  private _currentStatus: string;
+
   constructor(
-    private _id: number,
-    private _userId: number,
-    private _name: string,
-    private _specializations: string,
-    private _skillLevel: number,
-    private _currentStatus: string
-  ) {}
+    id: number,
+    userId: number,
+    name: string,
+    specializations: string,
+    skillLevel: number,
+    currentStatus: string
+  ) {
+    this._id = id;
+    this._userId = userId;
+    this._name = name;
+    this._specializations = specializations;
+    this._skillLevel = skillLevel;
+    this._currentStatus = currentStatus;
+  }
 
   get id(): number {
     return this._id;
