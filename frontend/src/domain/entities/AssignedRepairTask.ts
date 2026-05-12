01c0352079f5b834a207639d2c72ef96450a2019
Nikolay <nikolaydon2@gmail.com>
2026-05-12 09:49:05 +0300

    Added entity

---
+++ frontend/src/domain/entities/AssignedRepairTask.ts
@@ -0,0 +1,81 @@
+import { Technician } from "./Technician";
+
+export class AssignedRepairTask {
+  constructor(
+    private _id: number,
+    private _originalRequestId: number,
+    private _department: string,
+    private _floor: number,
+    private _room: number,
+    private _faultTypeCode: string,
+    private _category: string,
+    private _complexityLevel: number,
+    private _estimatedRepairMinutes: number,
+    private _priority: string,
+    private _assignedTechnicianId: number,
+    private _createdAt: number,
+    private _startedAt: string | null,
+    private _status: string,
+    private _technician: Technician
+  ) {}
+
+  get id(): number {
+    return this._id;
+  }
+
+  get originalRequestId(): number {
+    return this._originalRequestId;
+  }
+
+  get department(): string {
+    return this._department;
+  }
+
+  get floor(): number {
+    return this._floor;
+  }
+
+  get room(): number {
+    return this._room;
+  }
+
+  get faultTypeCode(): string {
+    return this._faultTypeCode;
+  }
+
+  get category(): string {
+    return this._category;
+  }
+
+  get complexityLevel(): number {
+    return this._complexityLevel;
+  }
+
+  get estimatedRepairMinutes(): number {
+    return this._estimatedRepairMinutes;
+  }
+
+  get priority(): string {
+    return this._priority;
+  }
+
+  get assignedTechnicianId(): number {
+    return this._assignedTechnicianId;
+  }
+
+  get createdAt(): number {
+    return this._createdAt;
+  }
+
+  get startedAt(): string | null {
+    return this._startedAt;
+  }
+
+  get status(): string {
+    return this._status;
+  }
+
+  get technician(): Technician {
+    return this._technician;
+  }
+}
