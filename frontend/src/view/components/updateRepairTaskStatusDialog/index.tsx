d312744af1a86c27a6612c51e4b435294bec22c8
Nikolay <nikolaydon2@gmail.com>
2026-05-14 14:16:50 +0300

    Created repair task status update dialog

--- frontend/src/view/components/updateRepairTaskStatusDialog/index.tsx
+++ frontend/src/view/components/updateRepairTaskStatusDialog/index.tsx
@@ -1,17 +1,33 @@
-import Button from '../../primitives/button';
-import Dialog from '../../primitives/dialog';
-import styles from './index.module.css';
+import Button from "../../primitives/button";
+import Dialog from "../../primitives/dialog";
+import styles from "./index.module.css";
+
 type UpdateRepairTaskStatusDialogProps = {
   isOpen: boolean;
   onClose: () => void;
+  onStartTask: () => Promise<void>;
+  onFinishTask: () => Promise<void>;
 };
-const UpdateRepairTaskStatusDialog = ({ isOpen, onClose }: UpdateRepairTaskStatusDialogProps) => {
-    return (  <Dialog isOpen={isOpen} title="" onClose={onClose}>
+
+const UpdateRepairTaskStatusDialog = ({
+  isOpen,
+  onClose,
+  onStartTask,
+  onFinishTask,
+}: UpdateRepairTaskStatusDialogProps) => {
+  return (
+    <Dialog
+      isOpen={isOpen}
+      title="Изменить статус задачи"
+      onClose={onClose}
+    >
       <div className={styles.content}>
-<Button > Начал ремонт </Button>
-<Button > Ремонт завершен </Button>
+        <Button onClick={onStartTask}>Начал ремонт</Button>
+
+        <Button onClick={onFinishTask}>Ремонт завершен</Button>
       </div>
     </Dialog>
-    );
-}
+  );
+};
+
 export default UpdateRepairTaskStatusDialog;
