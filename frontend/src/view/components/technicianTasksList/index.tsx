b121bb934e2c443ecd481e1b3bea445378f7786d
Nikolay <nikolaydon2@gmail.com>
2026-05-14 09:11:02 +0300

    Fixed bug on backend

---
+++ frontend/src/view/components/technicianTasksList/index.tsx
@@ -0,0 +1,32 @@
+const technicianTasksList = ({ tasks } : ) =>{
+    return(
+         <div>
+      <h2>Assigned repair tasks</h2>
+
+      {tasks.map((task) => (
+        <div key={task.id}>
+          <h3>{task.faultTypeCode}</h3>
+
+          <p>Task ID: {task.id}</p>
+          <p>Original request ID: {task.originalRequestId}</p>
+          <p>Department: {task.department}</p>
+          <p>Floor: {task.floor}</p>
+          <p>Room: {task.room}</p>
+          <p>Category: {task.category}</p>
+          <p>Complexity: {task.complexityLevel}</p>
+          <p>Estimated repair time: {task.estimatedRepairMinutes} minutes</p>
+          <p>Priority: {task.priority}</p>
+          <p>Status: {task.status}</p>
+
+          <h4>Technician</h4>
+          <p>Name: {task.technician.name}</p>
+          <p>Specializations: {task.technician.specializations}</p>
+          <p>Skill level: {task.technician.skillLevel}</p>
+          <p>Status: {task.technician.currentStatus}</p>
+
+          <hr />
+        </div>
+      ))}
+    </div>
+    )
+}
